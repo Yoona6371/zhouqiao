3765927fb15b5f58c100c5663218f12a0b3e4660
yoona <1347499464@qq.com>
2021-01-19 10:46:24 +0800

    eslint 格式化

--- src/router/nav.js
+++ src/router/nav.js
@@ -4,21 +4,21 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 // 该文件还没有链接到App.js上
 // 路由
-import Index from '../pages/home/index'
-import Tabbar from '../router/tabbar'
+import Index from '../pages/home/index';
+import Tabbar from '../router/tabbar';
 
 const Stack = createStackNavigator();
 
 function App() {
-    console.log("进入Nav")
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Tabbar" headerMode="none">
-                <Stack.Screen name="Index" component={Index} />
-                <Stack.Screen name="Tabbar" component={Tabbar} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  console.log('进入Nav');
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Tabbar" headerMode="none">
+        <Stack.Screen name="Index" component={Index} />
+        <Stack.Screen name="Tabbar" component={Tabbar} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
