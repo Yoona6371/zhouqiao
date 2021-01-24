13800b15b6616ee9e64b22b4d1735b9aea70934e
sword <1550185072@qq.com>
2021-01-24 18:18:11 +0800

    搜索框组件完成

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+
 class App extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return <View style={{margin:0,}} />;
+    return <View />;
   }
 }
 
