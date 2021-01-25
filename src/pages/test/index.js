af5e73798a04e7f9ba4e4a526ff9bfb8fa528892
Agan <1826251343@qq.com>
2021-01-25 10:58:29 +0800

    更新CommodityCard，RecordCard组件，属性下移

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import RecordCard from '../../components/bussiness/RecordCard';
+
 class App extends Component {
   constructor(props) {
     super(props);
