e4ac13863bf1ae16d03c5149a88dd029748fd684
小可爱 <2659580957@qq.com>
2021-03-05 23:32:28 +0800

    再次修改下拉刷新页面

--- src/pages/design/index.js
+++ src/pages/design/index.js
@@ -6,38 +6,37 @@ import RankCard from '../../components/bussiness/rankCard/rankCard';
 import { pxToDp } from '../../utils/pxToDp';
 import { flexColumnSpb, padding } from '../../utils/StyleUtils';
 import PullToRefresh from 'react-native-pull-refresh';
+import RefreshListView, { RefreshState } from 'react-native-refresh-list-view';
 
-import RefreshListView, {
-  RefreshState,
-} from '../../components/common/RefreshListView';
 export class index extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isRefreshing: false, // 下拉刷新
-      dataList: [], // 数据
-      refreshState: RefreshState.Idle,
-      totalPage: 0, // 总个数
-      currentPage: 1, // 当前页数
-      rankListOutOfThree: [],
-      top33: [
-        {
-          userAvatar: '',
-          nickName: '',
-          hot: '',
-        },
-        {
-          userAvatar: '',
-          nickName: '',
-          hot: '',
-        },
-        {
-          userAvatar: '',
-          nickName: '',
-          hot: '',
-        },
-      ],
-    };
+  state = {
+    isRefreshing: false, // 下拉刷新
+    dataList: [], // 数据
+    refreshState: RefreshState.Idle,
+    totalPage: 0, // 总个数
+    currentPage: 1, // 当前页数
+    rankListOutOfThree: [],
+    top33: [
+      {
+        userAvatar: '',
+        nickName: '',
+        hot: '',
+      },
+      {
+        userAvatar: '',
+        nickName: '',
+        hot: '',
+      },
+      {
+        userAvatar: '',
+        nickName: '',
+        hot: '',
+      },
+    ],
+  };
+
+  componentDidMount() {
+    this.onHeaderRefresh();
   }
 
   // 下拉刷新函数
@@ -45,8 +44,10 @@ export class index extends Component {
     this.setState({ isRefreshing: true });
     await this.getRankData(this.state.currentPage + 1);
     this.setState({
-      // 下拉刷新，一次只更新首页的5个数据; 如果向下滑动，更新10个数据
-      rankListOutOfThree: dataList.slice(3 + this.state.currentPage * 5, 4 + (this.state.currentPage + 1) * 5),
+      rankListOutOfThree: this.state.dataList.slice(
+        3 + this.state.currentPage * 5,
+        4 + (this.state.currentPage + 1) * 5,
+      ),
       currentPage: this.state.currentPage + 1,
       isRefreshing: false,
     });
@@ -55,28 +56,24 @@ export class index extends Component {
   getRankData = (page = 1) => {
     Http.getRankList({
       page: page,
-      size: 10,
+      size: 5,
     }).then((res) => {
       console.log(res.data.data);
       const dataList = [...this.state.dataList, ...res.data.data.dataList];
-      if(page == 1) {
-        this.setState({top33: dataList.slice(0, 3)});
+      if (page === 1) {
+        this.setState({ top33: dataList.slice(0, 3) });
       }
       this.setState({
         dataList: dataList,
         currentPage: res.data.data.currentPage,
         totalPage: res.data.data.totalRecords,
         rankListOutOfThree: dataList.slice(3),
-        }),
       });
     });
   };
-  componentDidMount() {
-    this.getRankData();
-  }
+
   onHeaderRefresh = async () => {
     this.setState({ refreshState: RefreshState.HeaderRefreshing });
-    console.log(1111);
     await this.getRankData();
     this.setState({
       refreshState:
@@ -88,20 +85,17 @@ export class index extends Component {
 
   onFooterRefresh = async () => {
     this.setState({ refreshState: RefreshState.FooterRefreshing });
-    const { totalPage, currentPage } = this.state;
-
-    let dataList = await this.getRankData(currentPage + 1);
+    await this.getRankData(this.state.currentPage + 1);
     this.setState({
-      dataList: dataList,
       refreshState:
-        dataList.length === totalPage
+        this.state.currentPage === this.state.totalPage
           ? RefreshState.NoMoreData
           : RefreshState.Idle,
     });
   };
 
   render() {
-    const { top3, rankListOutOfThree, top33 } = this.state;
+    const { rankListOutOfThree, top33 } = this.state;
     return (
       <PullToRefresh
         isRefreshing={this.state.isRefreshing}
