31194f9d32db875e7d0c21a2f9ec6cdea196e51e
小可爱 <2659580957@qq.com>
2021-03-05 22:42:18 +0800

    设计师榜单下拉刷新加载下一页

--- src/pages/design/index.js
+++ src/pages/design/index.js
@@ -5,119 +5,156 @@ import RankCardTop3 from '../../components/bussiness/rankCard/rankCardTop3';
 import RankCard from '../../components/bussiness/rankCard/rankCard';
 import { pxToDp } from '../../utils/pxToDp';
 import { flexColumnSpb, padding } from '../../utils/StyleUtils';
+import PullToRefresh from 'react-native-pull-refresh';
 
 import RefreshListView, {
   RefreshState,
 } from '../../components/common/RefreshListView';
 export class index extends Component {
-  state = {
-    dataList: [], // 数据
-    refreshState: RefreshState.Idle,
-    totalPage: 0, // 总个数
-    currentPage: 1, // 当前页数
-    rankListOutOfThree: [],
-    top33: [
-      {
-        userAvatar: '',
-        nickName: '',
-        hot: ''
-      },
-      {
-        userAvatar: '',
-        nickName: '',
-        hot: ''
-      },
-      {
-        userAvatar: '',
-        nickName: '',
-        hot: ''
-      }
-    ],
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRefreshing: false, // 下拉刷新
+      dataList: [], // 数据
+      refreshState: RefreshState.Idle,
+      totalPage: 0, // 总个数
+      currentPage: 1, // 当前页数
+      rankListOutOfThree: [],
+      top33: [
+        {
+          userAvatar: '',
+          nickName: '',
+          hot: '',
+        },
+        {
+          userAvatar: '',
+          nickName: '',
+          hot: '',
+        },
+        {
+          userAvatar: '',
+          nickName: '',
+          hot: '',
+        },
+      ],
+    };
+  }
 
-  };
+  // 下拉刷新函数
+  async onRefresh() {
+    this.setState({ isRefreshing: true });
+    await this.getRankData(this.state.currentPage + 1);
+    this.setState({
+      // 下拉刷新，一次只更新首页的5个数据; 如果向下滑动，更新10个数据
+      rankListOutOfThree: dataList.slice(3 + this.state.currentPage * 5, 4 + (this.state.currentPage + 1) * 5),
+      currentPage: this.state.currentPage + 1,
+      isRefreshing: false,
+    });
+  }
+
   getRankData = (page = 1) => {
     Http.getRankList({
       page: page,
-      size: 5,
-    }).then(res => {
-      console.log(res.data.data)
-      let dataList = res.data.data.dataList;
+      size: 10,
+    }).then((res) => {
+      console.log(res.data.data);
+      const dataList = [...this.state.dataList, ...res.data.data.dataList];
+      if(page == 1) {
+        this.setState({top33: dataList.slice(0, 3)});
+      }
       this.setState({
         dataList: dataList,
-        top33: dataList.slice(0, 3),
         currentPage: res.data.data.currentPage,
         totalPage: res.data.data.totalRecords,
-        rankListOutOfThree: dataList.filter((item, index) => {
-          return index != 0 && index != 1 && index != 2;
-        })
-      })
-    })
-  }
+        rankListOutOfThree: dataList.slice(3),
+        }),
+      });
+    });
+  };
   componentDidMount() {
     this.getRankData();
   }
   onHeaderRefresh = async () => {
     this.setState({ refreshState: RefreshState.HeaderRefreshing });
-    console.log(1111)
+    console.log(1111);
     await this.getRankData();
     this.setState({
       refreshState:
-        this.state.dataList.length < 1 ? RefreshState.EmptyData : RefreshState.Idle,
+        this.state.dataList.length < 1
+          ? RefreshState.EmptyData
+          : RefreshState.Idle,
     });
   };
 
   onFooterRefresh = async () => {
-    console.log(1332)
     this.setState({ refreshState: RefreshState.FooterRefreshing });
     const { totalPage, currentPage } = this.state;
 
+    let dataList = await this.getRankData(currentPage + 1);
+    this.setState({
+      dataList: dataList,
+      refreshState:
+        dataList.length === totalPage
+          ? RefreshState.NoMoreData
+          : RefreshState.Idle,
+    });
   };
+
   render() {
     const { top3, rankListOutOfThree, top33 } = this.state;
     return (
-      <View >
-        <View style={{ position: 'absolute', top: 0, zIndex: 999 }}>
-          <TopTitle title="设计师榜" showBtn={false} color="#FFF" bgColor="" />
-        </View>
-        <RankCardTop3
-          onPressChampion={() => alert(1111)}
-          onPressRunner_up={() => alert(222)}
-          onPressThird_place={() => alert(333)}
-          top3={top33}
-        >
-          {' '}
-        </RankCardTop3>
-        <ScrollView>
-          <RefreshListView
-            data={rankListOutOfThree}
-            numColumns={1}
-            contentContainerStyle={{ ...flexColumnSpb, backgroundColor: '' }}
-            keyExtractor={this.keyExtractor}
-            renderItem={({ item, index }) => (
-              <RankCard
-                userId={item.userId}
-                rankNumber={index + 4 < 10 ? '0' + (index + 4) : index + 4}
-                userPhoto={item.userAvatar}
-                userName={item.nickName}
-                hot={item.hot}
-                onPress={() => alert('哎，关注我')}
+      <PullToRefresh
+        isRefreshing={this.state.isRefreshing}
+        onRefresh={this.onRefresh.bind(this)}
+        animationBackgroundColor={'#564A63'}
+        pullHeight={180}
+        contentView={
+          <ScrollView style={{ flex: 1 }}>
+            <View style={{ position: 'absolute', top: 0, zIndex: 999 }}>
+              <TopTitle
+                title="设计师榜"
+                showBtn={false}
+                color="#FFF"
+                bgColor=""
               />
-            )}
-            refreshState={this.state.refreshState}
-            onHeaderRefresh={this.onHeaderRefresh}
-            onFooterRefresh={this.onFooterRefresh}
-            // 可选
-            footerRefreshingText="玩命加载中 >.<"
-            footerFailureText="我擦嘞，居然失败了 =.=!"
-            footerNoMoreDataText="-我是有底线的-"
-            footerEmptyDataText="-好像什么东西都没有-"
-          />
-
-        </ScrollView>
-      </View>
-    )
+            </View>
+            <RankCardTop3
+              onPressChampion={() => alert(1111)}
+              onPressRunner_up={() => alert(222)}
+              onPressThird_place={() => alert(333)}
+              top3={top33}
+            >
+              {' '}
+            </RankCardTop3>
+            <RefreshListView
+              data={rankListOutOfThree}
+              numColumns={1}
+              contentContainerStyle={{ ...flexColumnSpb, backgroundColor: '' }}
+              keyExtractor={this.keyExtractor}
+              renderItem={({ item, index }) => (
+                <RankCard
+                  userId={item.userId}
+                  rankNumber={index + 4 < 10 ? '0' + (index + 4) : index + 4}
+                  userPhoto={item.userAvatar}
+                  userName={item.nickName}
+                  hot={item.hot}
+                  onPress={() => alert('哎，关注我')}
+                />
+              )}
+              refreshState={this.state.refreshState}
+              onHeaderRefresh={this.onHeaderRefresh}
+              onFooterRefresh={this.onFooterRefresh}
+              // 可选
+              footerRefreshingText="玩命加载中 >.<"
+              footerFailureText="我擦嘞，居然失败了 =.=!"
+              footerNoMoreDataText="-我是有底线的-"
+              footerEmptyDataText="-好像什么东西都没有-"
+            />
+          </ScrollView>
+        }
+      />
+    );
   }
-
 }
 
 export default index;
