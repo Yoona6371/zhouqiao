22a0a265a7ff517674ec0aa4db12ff7967399148
EVA-Zero812 <68210918+EVA-Zero812@users.noreply.github.com>
2021-03-17 19:57:34 +0800

    Update index.js

--- src/pages/design/index.js
+++ src/pages/design/index.js
@@ -10,37 +10,34 @@ import RefreshListView, {
   RefreshState,
 } from '../../components/common/RefreshListView';
 export class index extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataList: [], // 数据
-      refreshState: RefreshState.Idle,
-      totalPage: 0, // 总个数
-      currentPage: 1, // 当前页数
-      rankListOutOfThree: [],
-      top33: [
-        {
-          userAvatar: '',
-          nickName: '',
-          hot: '',
-        },
-        {
-          userAvatar: '',
-          nickName: '',
-          hot: '',
-        },
-        {
-          userAvatar: '',
-          nickName: '',
-          hot: '',
-        },
-      ],
-    };
-  }
+  state = {
+    dataList: [], // 数据
+    refreshState: RefreshState.Idle,
+    totalPage: 0, // 总个数
+    currentPage: 1, // 当前页数
+    rankListOutOfThree: [],
+    top33: [
+      {
+        userAvatar: '',
+        nickName: '',
+        hot: '',
+      },
+      {
+        userAvatar: '',
+        nickName: '',
+        hot: '',
+      },
+      {
+        userAvatar: '',
+        nickName: '',
+        hot: '',
+      },
+    ],
+  };
   getRankData = (page = 1) => {
     Http.getRankList({
       page: page,
-      size: 10,
+      size: 5,
     }).then((res) => {
       console.log(res.data.data);
       let dataList = res.data.data.dataList;
@@ -50,7 +47,7 @@ export class index extends Component {
         currentPage: res.data.data.currentPage,
         totalPage: res.data.data.totalRecords,
         rankListOutOfThree: dataList.filter((item, index) => {
-          return index != 0 && index != 1 && index != 2;
+          return index !== 0 && index !== 1 && index !== 2;
         }),
       });
     });
@@ -71,23 +68,14 @@ export class index extends Component {
   };
 
   onFooterRefresh = async () => {
+    console.log(1332);
     this.setState({ refreshState: RefreshState.FooterRefreshing });
     const { totalPage, currentPage } = this.state;
-
-    let dataList = await this.getRankData(currentPage + 1);
-    this.setState({
-      dataList: dataList,
-      refreshState:
-        dataList.length === totalPage
-          ? RefreshState.NoMoreData
-          : RefreshState.Idle,
-    });
   };
-
   render() {
     const { top3, rankListOutOfThree, top33 } = this.state;
     return (
-      <View style={{ flex: 1 }}>
+      <View>
         <View style={{ position: 'absolute', top: 0, zIndex: 999 }}>
           <TopTitle title="设计师榜" showBtn={false} color="#FFF" bgColor="" />
         </View>
@@ -99,30 +87,32 @@ export class index extends Component {
         >
           {' '}
         </RankCardTop3>
-        <RefreshListView
-          data={rankListOutOfThree}
-          numColumns={1}
-          contentContainerStyle={{ ...flexColumnSpb, backgroundColor: '' }}
-          keyExtractor={this.keyExtractor}
-          renderItem={({ item, index }) => (
-            <RankCard
-              userId={item.userId}
-              rankNumber={index + 4 < 10 ? '0' + (index + 4) : index + 4}
-              userPhoto={item.userAvatar}
-              userName={item.nickName}
-              hot={item.hot}
-              onPress={() => alert('哎，关注我')}
-            />
-          )}
-          refreshState={this.state.refreshState}
-          onHeaderRefresh={this.onHeaderRefresh}
-          onFooterRefresh={this.onFooterRefresh}
-          // 可选
-          footerRefreshingText="玩命加载中 >.<"
-          footerFailureText="我擦嘞，居然失败了 =.=!"
-          footerNoMoreDataText="-我是有底线的-"
-          footerEmptyDataText="-好像什么东西都没有-"
-        />
+        <ScrollView>
+          <RefreshListView
+            data={rankListOutOfThree}
+            numColumns={1}
+            contentContainerStyle={{ ...flexColumnSpb, backgroundColor: '' }}
+            keyExtractor={this.keyExtractor}
+            renderItem={({ item, index }) => (
+              <RankCard
+                userId={item.userId}
+                rankNumber={index + 4 < 10 ? '0' + (index + 4) : index + 4}
+                userPhoto={item.userAvatar}
+                userName={item.nickName}
+                hot={item.hot}
+                onPress={() => alert('哎，关注我')}
+              />
+            )}
+            refreshState={this.state.refreshState}
+            onHeaderRefresh={this.onHeaderRefresh}
+            onFooterRefresh={this.onFooterRefresh}
+            // 可选
+            footerRefreshingText="玩命加载中 >.<"
+            footerFailureText="我擦嘞，居然失败了 =.=!"
+            footerNoMoreDataText="-我是有底线的-"
+            footerEmptyDataText="-好像什么东西都没有-"
+          />
+        </ScrollView>
       </View>
     );
   }
